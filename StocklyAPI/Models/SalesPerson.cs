03cacaec55411140dfaa327c97aa728c3535e926
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2025-02-15 01:49:30 -0300

    Adiciona parâmetros classe SalesPerson

--- StocklyAPI/Models/SalesPerson.cs
+++ StocklyAPI/Models/SalesPerson.cs
@@ -4,5 +4,12 @@ namespace StocklyAPI.Models
 {
     public class SalesPerson : IdentityUser
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int SalesCount { get; set; }
+        public decimal TotalSales { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool IsActive { get; set; }
+        public string ContactNumber { get; set; }
     }
 }
