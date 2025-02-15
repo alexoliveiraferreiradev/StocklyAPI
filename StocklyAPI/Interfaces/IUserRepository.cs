fa83bc524028dbd0dfe17a5a98792eb0d97da1d6
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2025-02-15 01:00:13 -0300

    Realizando CRUD no IUserRepository

--- StocklyAPI/Interfaces/IUserRepository.cs
+++ StocklyAPI/Interfaces/IUserRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace StocklyAPI.Interfaces
+﻿using StocklyAPI.Models;
+
+namespace StocklyAPI.Interfaces
 {
-    public interface IUserRepository
+    public interface IUserRepository<T> where T : User
     {
+        Task Add(T pUser);
+        Task Update(T pUser);
+        Task Delete(T pUser);
+        Task<T> Get(int pId);
+
     }
 }
