a992ae79d4b6a508f5bbaabb101836c4cae1e86a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2025-02-15 16:03:37 -0300

    Adiciona OrderRepository e altera CustomerRepository

---
+++ StocklyAPI/Data/Repository/OrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocklyAPI.Interfaces;
+using StocklyAPI.Models;
+
+namespace StocklyAPI.Data.Repository
+{
+    public class OrderRepository : IOrderRepository<Order>
+    {
+        private readonly StocklyDbContext _context;
+        public OrderRepository(StocklyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Order pOrder)
+        {
+            _context.Orders.Add(pOrder);
+            await _context.SaveChangesAsync();  
+        }
+
+        public async Task Delete(Order pOrder)
+        {
+            _context.Orders.Remove(pOrder);
+            await _context.SaveChangesAsync();  
+        }
+
+        public async Task<Order> Get(int id)
+        {
+           return await _context.Orders.Where(
+               x=>x.Id.Equals(id)).FirstOrDefaultAsync() ?? new Order();
+        }
+
+        public async Task<IEnumerable<Order>> GetAll()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task Update(Order pNewOrder)
+        {
+            var order = await Get(pNewOrder.Id);
+            if(order != null)
+            {
+                _context.Entry(order).CurrentValues.SetValues(pNewOrder);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ StocklyAPI/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using StocklyAPI.Models;
+
+namespace StocklyAPI.Interfaces
+{
+    public interface ICustomerRepository<T> where T : Customer
+    {
+        Task Add(T pUser);
+        Task Update(T pUser);
+        Task Delete(T pUser);
+        Task<T> Get(int id);
+    }
+}
