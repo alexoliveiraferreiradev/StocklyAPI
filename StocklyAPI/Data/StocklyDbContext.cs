a732349c36ad53ce3acd25e8cbc9fff17af32b69
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2025-02-15 01:30:11 -0300

    Adiciona product no DbSet

--- StocklyAPI/Data/StocklyDbContext.cs
+++ StocklyAPI/Data/StocklyDbContext.cs
@@ -7,9 +7,10 @@ namespace StocklyAPI.Data
     {
         public StocklyDbContext()
         {
-            
+
         }
 
-        public DbSet<Admin> Admins { get; set; }    
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
