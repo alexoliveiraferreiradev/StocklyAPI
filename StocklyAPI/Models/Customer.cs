05d9cae8c73a2a4c1895d9c7383818a82504dfb1
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2025-02-15 01:46:55 -0300

    Adiciona parâmetros classe Customer

--- StocklyAPI/Models/Customer.cs
+++ StocklyAPI/Models/Customer.cs
@@ -4,5 +4,12 @@ namespace StocklyAPI.Models
 {
     public class Customer : IdentityUser
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ShippingAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public List<Order> Orders { get; set; }
+        public bool IsActive { get; set; }
     }
 }
