a54399efd8438853204c8e67df6691ba85b22584
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2025-02-15 16:57:41 -0300

    Adiciona AdminConfiguration

---
+++ StocklyAPI/Data/Configuration/AdminConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StocklyAPI.Models;
+
+namespace StocklyAPI.Data.Configuration
+{
+    public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+    {
+        public void Configure(EntityTypeBuilder<Admin> builder)
+        {
+            builder.ToTable("Admins");
+
+            builder.Property(x => x.Id)
+                .HasColumnName("Admin_Id");
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("nvarchar(100)");
+
+            builder.Property(x=>x.RegistrationDate)
+                .IsRequired();
+
+            builder.Property(x => x.HasElevatedPrivileges)
+                .IsRequired()
+                .HasDefaultValue(true);
+
+
+        }
+    }
+}
